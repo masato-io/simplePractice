6574b5c14d60fd4b3c1f69ecafa93e5c268e9072
masato-io <hi@masato.io>
2020-10-04 16:47:58 -0700

    Add unit test for office model

--- tests/unit/models/office-test.js
+++ tests/unit/models/office-test.js
@@ -1,13 +1,26 @@
-import { module, test } from 'qunit';
-import { setupTest } from 'ember-qunit';
+import { module, test } from "qunit";
+import { setupTest } from "ember-qunit";
 
-module('Unit | Model | office', function(hooks) {
+module("Unit | Model | office", function (hooks) {
   setupTest(hooks);
 
   // Replace this with your real tests.
-  test('it exists', function(assert) {
-    let store = this.owner.lookup('service:store');
-    let model = store.createRecord('office', {});
-    assert.ok(model);
+  test("it exists", function (assert) {
+    let store = this.owner.lookup("service:store");
+    let office = store.createRecord("office", {
+      name: "Main Office",
+      street: "Elise Drive",
+      city: "Port Alyssonbury",
+      state: "NV",
+      zip: "51859",
+      phone: "(626) 298-1956",
+    });
+
+    assert.equal(office.name, "Main Office");
+    assert.equal(office.street, "Elise Drive");
+    assert.equal(office.city, "Port Alyssonbury");
+    assert.equal(office.state, "NV");
+    assert.equal(office.zip, "51859");
+    assert.equal(office.phone, "(626) 298-1956");
   });
 });
