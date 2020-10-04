e05b218ed6a0633c817fb64934d94cf776252943
masato-io <hi@masato.io>
2020-10-03 19:46:57 -0700

    Add tailwind for styling

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,10 +1,17 @@
-'use strict';
+"use strict";
 
-const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const EmberApp = require("ember-cli/lib/broccoli/ember-app");
 
-module.exports = function(defaults) {
+module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    // sassOptions: {
+    //   extension: "scss",
+    // },
+    postcssOptions: {
+      compile: {
+        plugins: [require("tailwindcss")],
+      },
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
