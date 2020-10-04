467d5cd78f6a7183f6b2586d5c3874cfb83c1ac1
masato-io <hi@masato.io>
2020-10-04 16:13:41 -0700

    Add integration test for office component

--- tests/integration/components/office-test.js
+++ tests/integration/components/office-test.js
@@ -7,15 +7,22 @@ module("Integration | Component | office", function (hooks) {
   setupRenderingTest(hooks);
 
   test("it renders", async function (assert) {
-    // const service = this.owner
-    //   .lookup("service:store")
-    //   .createRecord("cpt-code", {
-    //     description: "Group Therapy",
-    //     duration: 50,
-    //     rate: 300,
-    //   });
-    // this.set("service", service);
-    // await render(hbs`<Office @service={{this.service}} />`);
-    // assert.equal(this.element.textContent.trim(), "");
+    const office = this.owner.lookup("service:store").createRecord("office", {
+      name: "Main Office",
+      street: "Elise Drive",
+      city: "Port Alyssonbury",
+      state: "NV",
+      zip: "51859",
+      phone: "(626) 298-1956",
+    });
+    this.set("office", office);
+    await render(hbs`<Office @office={{this.office}}/>`);
+
+    assert.dom("[data-test-name]").hasText("Main Office");
+    assert.dom("[data-test-street]").hasText("Elise Drive");
+    assert
+      .dom("[data-test-city-state-zip]")
+      .hasText("Port Alyssonbury, NV 51859");
+    assert.dom("[data-test-phone]").hasText("(626) 298-1956");
   });
 });
