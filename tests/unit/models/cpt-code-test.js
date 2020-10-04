7b52dfac12d520e63b516f1abbbac0a5a0fb9720
masato-io <hi@masato.io>
2020-10-04 16:46:18 -0700

    Add unit test for cpt-code model

--- tests/unit/models/cpt-code-test.js
+++ tests/unit/models/cpt-code-test.js
@@ -1,13 +1,20 @@
-import { module, test } from 'qunit';
-import { setupTest } from 'ember-qunit';
+import { module, test } from "qunit";
+import { setupTest } from "ember-qunit";
 
-module('Unit | Model | cpt code', function(hooks) {
+module("Unit | Model | cpt code", function (hooks) {
   setupTest(hooks);
 
   // Replace this with your real tests.
-  test('it exists', function(assert) {
-    let store = this.owner.lookup('service:store');
-    let model = store.createRecord('cpt-code', {});
-    assert.ok(model);
+  test("it exists", function (assert) {
+    let store = this.owner.lookup("service:store");
+    let service = store.createRecord("cpt-code", {
+      description: "Group Therapy",
+      duration: 50,
+      rate: 300,
+    });
+
+    assert.equal(service.description, "Group Therapy");
+    assert.equal(service.duration, 50);
+    assert.equal(service.rate, 300);
   });
 });
